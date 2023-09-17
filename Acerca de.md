ae3fcf8206f100fa607e84cb32d05b6df4a94958
Elkin Rodriguez <elkin.rodrigueze@outlook.com>
2023-09-17 18:11:06 -0500

    fin clase 11 del curso, y ejecucion web and desktop

---
+++ Acerca de.md
@@ -0,0 +1,21 @@
+# Curso de Flet
+
+1. Enlace del curso:
+
+https://www.youtube.com/watch?v=JT5_zM-pkSU&list=PL2PZw96yQChyD79_mtqY_oBdyiCdRJnJa&index=7
+
+* Notas: Voy en clase 12
+
+## Primeros pasos:
+
+1. Instalar ambiente virtual con librería de python desde la terminal:
+
+* Ir a la ruta donde voy a instalar el ambiente virtual
+* python -m venv venv
+* Activar el ambiente: .\venv\Scripts\activate
+* Desactivar: deactivate (solo escribir la palabra y sale)
+
+2. Crear requirements.txt --> (validar para qué):
+
+- Desde la ruta del curso
+- pip freeze > requirements.txt
---
+++ demo-inicial-contador/counter.py
@@ -0,0 +1,33 @@
+import flet
+from flet import IconButton, Page, Row, TextField, icons # elementos y clases de este modulo
+
+def main(page: Page):
+    page.title = 'Flet Counter'
+    page.vertical_alignment = 'center'
+
+    txt_number = TextField(value='0', text_align='right', width=100)
+
+    def minus_click(event):
+        print('minus click')
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(event):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        Row(
+            [
+                IconButton(icons.REMOVE, on_click=minus_click),txt_number,IconButton(icons.ADD, on_click=plus_click)
+            ],
+            alignment='center'
+        )
+    )
+
+
+# Modo Desktop:
+flet.app(target=main)
+
+# Modo Web
+#flet.app(target=main, view=flet.WEB_BROWSER)
